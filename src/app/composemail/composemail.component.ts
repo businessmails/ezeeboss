d4a6b916e9ec90465eeeed735ed40a0c3f672b85
atulkhandar <hubcityindia@gmail.com>
2019-07-24 21:58:29 +0530

    function

--- src/app/composemail/composemail.component.ts
+++ src/app/composemail/composemail.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit,ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { AuthenticationService, UserDetails, TokenPayload} from '../authentication.service';
+import { AuthenticationService, UserDetails, TokenPayload } from '../authentication.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { AppComponent} from '../app.component';
+import { AppComponent } from '../app.component';
 import { Router } from '@angular/router'
 @Component({
   selector: 'app-composemail',
@@ -10,26 +10,28 @@ import { Router } from '@angular/router'
   styleUrls: ['./composemail.component.css']
 })
 export class ComposemailComponent implements OnInit {
-   details: UserDetails;
-   fullname: String;
-   userid: string;
-   useremail:string;
-   email: string;
-   loading = false;
-   filename:string;
-   emailerror:string;
-   filesToUpload: Array<File> = [];
-   filesname = [];
-   editorContent: string
-   formData: FormData = new FormData();
-   @ViewChild('toemail') toemail: ElementRef;
-   @ViewChild('subject') subject: ElementRef;
-  hide: boolean =false;
-
+  details: UserDetails;
+  fullname: String;
+  userid: string;
+  useremail: string;
+  email: string;
+  loading = false;
+  filename: string;
+  emailerror: string;
+  filesToUpload: Array<File> = [];
+  filesname = [];
+  editorContent: string
+  formData: FormData = new FormData();
+  @ViewChild('toemail') toemail: ElementRef;
+  @ViewChild('subject') subject: ElementRef;
+  hide: boolean = false;
+  serchedmail: any=null;
+  result: any;
+showsearch=false;
   constructor(
     private http: HttpClient,
     private auth: AuthenticationService,
-    private AppComponent:AppComponent,
+    private AppComponent: AppComponent,
     private router: Router
   ) { }
 
@@ -42,106 +44,122 @@ export class ComposemailComponent implements OnInit {
 
     });
   }
+  search(email) {
+    // alert(email)
 
-  fileChange(fileInput: any) {
-  
-    this.hide=true;
-       this.filesToUpload = <Array<File>>fileInput.target.files;
-       const fileList: FileList = fileInput.target.files;
-       const files: Array<File> = this.filesToUpload;
-    for(let i =0; i < files.length; i++){
-        this.formData.append("uploads[]", files[i], files[i]['name']);
+    var mailarray= email.split(',');
+email=mailarray[mailarray.length-1];
+    if (email != '') {
+      this.showsearch=true;
+      this.http.post(this.AppComponent.BASE_URL + '/api/serchmail', { email: email })
+        .subscribe(data => {
+          this.result = data;
+          this.serchedmail = this.result.result;
+        });
+    }else{
+       this.showsearch=false;
+      this.serchedmail =[]
     }
-       for(var i = 0;i<fileList.length;i++) {
-       const file: File = fileList[i];
-       this.filesname.push({filename:file.name});
-       }
+
+  }
+  fileChange(fileInput: any) {
+
+    this.hide = true;
+    this.filesToUpload = <Array<File>>fileInput.target.files;
+    const fileList: FileList = fileInput.target.files;
+    const files: Array<File> = this.filesToUpload;
+    for (let i = 0; i < files.length; i++) {
+      this.formData.append("uploads[]", files[i], files[i]['name']);
+    }
+    for (var i = 0; i < fileList.length; i++) {
+      const file: File = fileList[i];
+      this.filesname.push({ filename: file.name });
+    }
   }
 
   removeattachment(name) {
-    var attachfiles=[];
-    attachfiles=this.formData.getAll("uploads[]"); 
-  
-    for(var i=0;i<attachfiles.length;i++) {
-    //  alert(this.filesname[i].filename)
-      if(this.filesname[i].filename === name) {
-        this.filesname.splice(i,1);
+    var attachfiles = [];
+    attachfiles = this.formData.getAll("uploads[]");
+
+    for (var i = 0; i < attachfiles.length; i++) {
+      //  alert(this.filesname[i].filename)
+      if (this.filesname[i].filename === name) {
+        this.filesname.splice(i, 1);
       }
-			if(attachfiles[i].name === name) {
-				attachfiles.splice(i,1);
-				break;
+      if (attachfiles[i].name === name) {
+        attachfiles.splice(i, 1);
+        break;
       }
-     
+
     }
-      this.formData.delete("uploads[]");
-      for(let i =0; i < attachfiles.length; i++){
-        this.formData.append("uploads[]", attachfiles[i], attachfiles[i]['name']);
+    this.formData.delete("uploads[]");
+    for (let i = 0; i < attachfiles.length; i++) {
+      this.formData.append("uploads[]", attachfiles[i], attachfiles[i]['name']);
     }
-    if(attachfiles.length==0){
-this.hide=false;
-    }  
+    if (attachfiles.length == 0) {
+      this.hide = false;
+    }
   }
 
   checkEmail(email) {
     var regExp = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     return regExp.test(email);
-    }
-    
-   validateemail() {
+  }
+
+  validateemail() {
     var emails = this.toemail.nativeElement.value;
     var emailArray = emails.split(",");
     var invEmails = "";
-//	console.log("emailArray-",emailArray)
-    for(var i = 0; i <= (emailArray.length - 1); i++){
-      if(this.checkEmail(emailArray[i])){
+    //	console.log("emailArray-",emailArray)
+    for (var i = 0; i <= (emailArray.length - 1); i++) {
+      if (this.checkEmail(emailArray[i])) {
         this.emailerror = ''
-            } else{
-              invEmails += emailArray[i].toLowerCase(); + "\n";
+      } else {
+        invEmails += emailArray[i].toLowerCase(); + "\n";
       }
     }
-    if(invEmails != ""){
+    if (invEmails != "") {
       this.emailerror = 'Invalid Email: ' + invEmails
-     // alert("Invalid emails:\n" + invEmails);
+      // alert("Invalid emails:\n" + invEmails);
     }
-   } 
+  }
 
   sendsmartmail() {
-      if(this.toemail.nativeElement.value === '')
-      {
+    if (this.toemail.nativeElement.value === '') {
       this.emailerror = 'Enter a valid Email'
-      }
-      else {
-        var emails = this.toemail.nativeElement.value;
-        var emailArray = emails.split(",");
-        var invEmails = "";
-        for(var i = 0; i <= (emailArray.length - 1); i++){
-          if(this.checkEmail(emailArray[i])){
-            
-                } else{
-                  invEmails += emailArray[i] + "\n";
-          }
-        }
-        if(invEmails != ""){
-          this.emailerror = 'Invalid Email: ' + invEmails
-         // alert("Invalid emails:\n" + invEmails);
+    }
+    else {
+      var emails = this.toemail.nativeElement.value;
+      var emailArray = emails.split(",");
+      var invEmails = "";
+      for (var i = 0; i <= (emailArray.length - 1); i++) {
+        if (this.checkEmail(emailArray[i])) {
+
         } else {
-            this.loading = true;
-            this.formData.append('from',this.userid);
-            this.formData.append('data',this.editorContent);
-            this.formData.append('fromemail',this.email);
-            this.formData.append('toemail',this.toemail.nativeElement.value.toLowerCase());
-            this.formData.append('subject',this.subject.nativeElement.value);
-            this.http.post(this.AppComponent.BASE_URL+'/api/sendsmartmail', this.formData)
-                .subscribe(data => {
+          invEmails += emailArray[i] + "\n";
+        }
+      }
+      if (invEmails != "") {
+        this.emailerror = 'Invalid Email: ' + invEmails
+        // alert("Invalid emails:\n" + invEmails);
+      } else {
+        this.loading = true;
+        this.formData.append('from', this.userid);
+        this.formData.append('data', this.editorContent);
+        this.formData.append('fromemail', this.email);
+        this.formData.append('toemail', this.toemail.nativeElement.value.toLowerCase());
+        this.formData.append('subject', this.subject.nativeElement.value);
+        this.http.post(this.AppComponent.BASE_URL + '/api/sendsmartmail', this.formData)
+          .subscribe(data => {
             this.loading = false;
             alert('Mail Sent Successfully');
             this.router.navigateByUrl('/sentmail');
-                });
-        }
-    
+          });
+      }
+
+    }
   }
-}
-logout() {
-  this.auth.logout();
-}
+  logout() {
+    this.auth.logout();
+  }
 }
