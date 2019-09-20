899f482113839c337b77bf5445ecc750e0f0ec92
jagveersinghthiara <work.jagveer@gmail.com>
2019-09-20 19:36:52 +0530

    changes

--- src/app/completed-doc/completed-doc.component.ts
+++ src/app/completed-doc/completed-doc.component.ts
@@ -24,13 +24,20 @@ export class CompletedDocComponent implements OnInit {
    documentdetail:any;
    documents:any;
    videourl:string;
+    pageOfItems: Array<any>;
+  checkeddocuments: any;
+  checkedid: any[];
+  isSelected: boolean;
   constructor(
     private http: HttpClient,
     private auth: AuthenticationService,
     private AppComponent:AppComponent
 
   ) { }
-
+ onChangePage(pageOfItems: Array<any>) {
+        // update current page of items
+        this.pageOfItems = pageOfItems;
+    }
   ngOnInit() {
     this.auth.profile().subscribe(user => {
       this.details = user;
@@ -56,4 +63,53 @@ export class CompletedDocComponent implements OnInit {
   setvideourl(url) {
     this.videourl = url;
     }
+
+  checkAll(ev) {
+    this.documents.forEach(x => x.selected = ev.target.checked);
+    this.isSelected = true;
+    //  alert(this.isSelected)
+  }
+
+  isAllChecked() {
+    if(this.isSelected == true)
+    // alert(this.isSelected)
+    return this.documents.every(_ => _.selected);
+  }
+    deletedocument() {
+      alert()
+    this.checkeddocuments = this.documents.filter(_ => _.selected);
+ 
+    if ( this.checkeddocuments.length == 0 ) {
+      alert('Please Check At Least One Record');
+    }
+    else {
+      if (confirm('Are you sure to delete the selected records')) {
+        this.checkedid = [];
+       for ( let i = 0; i < this.checkeddocuments.length; i++) {
+        this.checkedid.push({_id: this.checkeddocuments[i]._id});
+       }
+       this.http.post( this.AppComponent.BASE_URL+'/api/deletemydocuments', {documentid: this.checkedid})
+       .subscribe(data => {
+        this.http.post(this.AppComponent.BASE_URL+'/api/doccompletedbyme' , {useremail :this.useremail})
+        .subscribe(data => {
+          console.log("...\...")
+        this.documentdetail = data;
+        this.documents = this.documentdetail.message;
+          if(this.documents == ''){
+            $('#checkId').hide();
+            $('#DeleteID').hide();
+            $('#completeRequiredId').hide();
+           }
+           else{
+            $('#checkId').show();
+            $('#DeleteID').show();
+            $('#completeRequiredId').show();
+          }
+        });
+        
+       });
+    }
+
+    }
+  }
 }
